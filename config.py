4db80535da3fd3d49b6cfcd66f922c81579d4bd5
Wesley Tokoi <tokoiwesley@gmail.com>
2022-06-18 11:55:31 +0300

    Connect to a local postgresql database

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ DEBUG = True
 # Connect to the database
 
 
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+# IMPLEMENT DATABASE URL
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:abc@localhost:5432/fyyurapp'
